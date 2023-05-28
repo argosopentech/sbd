fa9d5520308be8d3f6861726db0d55d797216233
P.J. Finlay <pjappdevelopment@gmail.com>
2023-05-28 11:13:36 -0500

    Add benchmarks for different SBD libraries

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # sbd
 Sentence boundary detection for machine translation
 
+## Benchmarks
+| Model                  | Average Accuracy  | Average Runtime (seconds) |
+| ---------------------- | ----------------- | ------------------------- |
+| Spacy en_core_web_sm   | 0.924311498164287 | 0.0250468651453654        |
+| Spacy xx_sent_ud_sm    | 0.924311498164287 | 0.00476229190826416       |
+| Argos Translate 2 Beta | 0.515548280365557 | 1.87798078854879          |
+| Stanza en              | 0.924311498164287 | 0.0219400326410929        |
 
-# Stanza
-"en" model size = 428M
+#### Libraries tested
+- [Argos Translate 2 beta](https://github.com/argosopentech/argos-translate/tree/v2)
+- [Stanza](https://stanfordnlp.github.io/stanza/)
+- [https://spacy.io/](https://spacy.io/)
 
-Installs to `$HOME/stanza_resources`
+#### Data
+The data is from Wikipedia and I (native English speaker) manually split the sentences. Currently only English data is used but this script can easily be extended for more languages.
+
+https://en.wikipedia.org/
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from typing import List
+import csv
+import json
+from difflib import SequenceMatcher
+import time
+from collections import defaultdict
 
 import argostranslate.translate
 import spacy
@@ -24,20 +29,6 @@ class SpacySentenceBoundaryDetectionModel(ISentenceBoundaryDetectionModel):
 
 
 # Spacy sentence boundary detection Sentencizer
-class SpacySentencizer(ISentenceBoundaryDetectionModel):
-    def __init__(self):
-        self.nlp = spacy.load("en_core_web_sm")
-        self.nlp.add_pipe("sentencizer")
-
-    def split_sentences(self, text: str, lang_code: str) -> List[str]:
-        doc = self.nlp(text)
-        return [sent.text for sent in doc.sents]
-
-    def __str__(self):
-        return "Spacy Sentencizer en_core_web_sm"
-
-
-# Spacy sentence boundary detection Sentencizer
 # https://community.libretranslate.com/t/sentence-boundary-detection-for-machine-translation/606/3
 class SpacySentencizerSmall(ISentenceBoundaryDetectionModel):
     def __init__(self):
@@ -49,7 +40,7 @@ class SpacySentencizerSmall(ISentenceBoundaryDetectionModel):
         return [sent.text for sent in doc.sents]
 
     def __str__(self):
-        return "Spacy Sentencizer en_core_web_sm"
+        return "Spacy xx_sent_ud_sm"
 
 
 class ArgosTranslateSBD(ISentenceBoundaryDetectionModel):
@@ -75,19 +66,83 @@ class StanzaSBD(ISentenceBoundaryDetectionModel):
 
 sbd_models = [
     SpacySentenceBoundaryDetectionModel(),
-    SpacySentencizer(),
     SpacySentencizerSmall(),
     ArgosTranslateSBD(),
     StanzaSBD(),
 ]
-data = [
-    "The black-and-yellow broadbill (Eurylaimus ochromalus) is a species of bird in Eurylaimidae, the typical broadbill family. It is small, with a black head, breastband, and upperparts, a white neckband, yellow streaking on the back and wings, and wine-pink underparts that turn yellow towards the belly. The beak is bright blue, with a green tip to the upper mandible and black edges. The black breastband is incomplete in females. The black-and-yellow broadbill occurs in Brunei, Indonesia, Malaysia, Myanmar, Singapore, and Thailand, in lowland forests up to an elevation of 1,220 m (4,000 ft). It is mainly insectivorous, but also eats molluscs and some fruit. It breeds during the dry season with both sexes helping build a large, untidy nest from moss, fungal mycelia, and leaves. The clutch is usually 2–3 eggs, and sometimes includes a fourth runt egg. They are incubated by both sexes. The species is listed as near-threatened by the IUCN due to a decline in its population caused by habitat loss. (Full article...)"
-]
+
+
+# Get testing data
+# Testing data in sbd-testing-data.jsonl
+# Example line:
+# {"lang_code": "en", "q": "This is test data. This is another sentence.", "split_sentences": ["This is test data. ", "This is another sentence."]]}
+
+TEST_DATA_FILE = "sbd-testing-data.jsonl"
+
+data = []
+with open(TEST_DATA_FILE, "r") as f:
+    for line in f:
+        data.append(json.loads(line))
+        
+# Test models
+class Experiment:
+    def __init__(self, model_name: str, similarity: float, run_time: float):
+        self.model_name = model_name
+        self.similarity = similarity
+        self.run_time = run_time
+
+    def __str__(self) -> str:
+        return f"{self.model_name}: similarity: {self.similarity} runtime: {self.run_time}"
+
+def similarity_of_str_lst(expected: List[str], actual: List[str]) -> float:
+    if len(expected) == 0 or len(actual) == 0:
+        return 0.0
+    unmatched_expected = expected.copy()
+    similarity_sum = 0.0
+    for actual_sentence in actual:
+        # Find best matching expected sentence
+        best_match = None
+        best_match_ratio = 0
+        for expected_sentence in unmatched_expected:
+            ratio = SequenceMatcher(None, expected_sentence, actual_sentence).ratio()
+            if ratio > best_match_ratio:
+                best_match = expected_sentence
+                best_match_ratio = ratio
+        # Remove best match from unmatched expected sentences
+        if best_match is not None:
+            unmatched_expected.remove(best_match)
+        
+        # Sum the similarities by sentence
+        similarity_sum += best_match_ratio
+
+    return similarity_sum / len(max(expected, actual, key=len))
+
+experiments = defaultdict(list)
 
 for model in sbd_models:
-    print(f"Model: {model}")
-    for text in data:
-        print(f"Input: {text}")
-        print(f"Output: {model.split_sentences(text, 'en')}")
-        print()
-    print()
+    print(f"Testing {model}")
+    for test in data:
+        expected = test["split_sentences"]
+        start_time = time.time()
+        actual = model.split_sentences(test["q"], test["lang_code"])
+        end_time = time.time()
+        run_time = end_time - start_time
+        similarity = similarity_of_str_lst(expected, actual)
+        experiment = Experiment(str(model), similarity, run_time)
+        experiments[str(model)].append(experiment)
+
+compiled_data = list()
+
+data_headers = ["Model", "Average Accuracy", "Average Runtime (seconds)"]
+
+for model, experiments in experiments.items():
+    average_accuracy = sum([experiment.similarity for experiment in experiments]) / len(experiments)
+    average_runtime = sum([experiment.run_time for experiment in experiments]) / len(experiments)
+    compiled_data.append([model, average_accuracy, average_runtime])
+    
+# Write to csv
+with open("sbd-testing-results.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(data_headers)
+    writer.writerows(compiled_data)
+print("Compiled results written to sbd-testing-results.csv")
